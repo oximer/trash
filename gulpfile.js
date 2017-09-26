c7b9f8af88389f12a45fd718cc37ba8b35e8002e
oximer <oximer@gmail.com>
2017-09-26 19:07:39 -0300

    Adicionando gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const exec = require('child_process').exec;
+const gulp = require('gulp');
+
+gulp.task('updateGit', cb => {
+  exec('git push --prune git@gitlab.com:oximer/trash.git +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*', err => {
+    console.log('updateGit', err);
+    cb();
+  });
+});
+
+gulp.task('post-commit', ['updateGit']);
---
+++ lixo.js
@@ -0,0 +1,2 @@
+Lixo
+lixoso
